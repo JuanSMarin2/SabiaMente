2092942591d6db4ac8b3884cb3c42f184405ee99
JuanSMarin2 <160736940+JuanSMarin2@users.noreply.github.com>
2025-08-22 18:04:35 -0500

    Creacion del proyecto y configuracion de react - vite

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function App() {
+  const goLogin = () => {
+    window.location.href = "/login.html";
+  };
+
+  return (
+    <div className="page">
+      <main className="card">
+        <img
+          src="/img/logo.png"
+          alt="SabiaMente Logo"
+          className="logo"
+          draggable="false"
+        />
+        <h1 className="title"></h1>
+        <p className="subtitle">
+          Entrena tu mente
+          <br />
+          Cuida tu vida
+        </p>
+
+        <button className="primaryBtn" onClick={goLogin}>
+          Comenzar
+        </button>
+      </main>
+
+      {/* Botón flotante "i" (abajo-izquierda) */}
+      <a className="fab" href="/about.html" aria-label="Acerca de">
+        i
+      </a>
+    </div>
+  );
+}
